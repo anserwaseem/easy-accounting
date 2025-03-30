47ee985e4eebc5caf1fbf1857704bd11d787d00b
Anser Waseem <hafiz.anser.waseem@gmail.com>
2025-03-30 13:13:13 +0500

    add reports route (#71)

--- src/renderer/routes.tsx
+++ src/renderer/routes.tsx
@@ -23,6 +23,7 @@ import InvoicePage from './views/Invoice';
 import { InvalidRoute } from './views/InvalidRoute';
 import PrintableInvoiceScreen from './views/PrintableInvoiceScreen';
 import ReportsPage from './views/Reports';
+import TrialBalancePage from './views/Reports/TrialBalance';
 
 const AppRoutes: React.FC = () => (
   <ThemeProvider>
@@ -45,6 +46,10 @@ const AppRoutes: React.FC = () => (
                 <Route path=":id" element={<JournalPage />} />
               </Route>
               <Route path="inventory" element={<InventoryPage />} />
+              <Route path="reports">
+                <Route index element={<ReportsPage />} />
+                <Route path="trial-balance" element={<TrialBalancePage />} />
+              </Route>
               <Route path="purchase/invoices">
                 <Route
                   index
